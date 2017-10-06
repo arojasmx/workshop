cf63a3c23aec1048fb235d45ac1acbc1be205fd1
Rojas Rodriguez <a.a.rojas.rodriguez@accenture.com>
2017-10-06 13:03:26 -0500

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="app-header">
+        Plaza Godinez - Fast Food
+    </div>
+</template>
+
+<style>
+    .app-header {
+        height: 50px;
+        background-color: #08b4e4;
+        padding: 13px;
+        box-sizing: border-box;
+        font-weight: bold;
+        color: white;
+    }
+</style>
